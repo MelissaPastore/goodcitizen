94091972e52c23c396f85cd4c93dbc5a879b3a02
Melissa Pastore <melissa.pastore@gmail.com>
2020-12-29 23:26:49 -0500

    can look up reps using address

---
+++ src/RepInfo.js
@@ -0,0 +1,51 @@
+const RepInfo = (props) => {
+  const repInfo = props.repInfo;
+  const offices = repInfo.offices;
+  const officials = repInfo.officials;
+
+  return (
+    <div>
+      <p>Here are your representatives:</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Office</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {officials.map((official, index) => {
+            let office = offices.find((office) => {
+              return office.officialIndices.includes(index);
+            });
+
+            return (
+              <tr key={index}>
+                <td>
+                  {official.photoUrl ? (
+                    <img
+                      className="rep-img"
+                      src={official.photoUrl}
+                      alt={`${official.name}`}
+                    />
+                  ) : (
+                    ""
+                  )}
+                </td>
+                <td>{official.name}</td>
+                <td>{office.name}</td>
+                <td>
+                  {official.emails ? official.emails[0] : "No email provided"}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default RepInfo;
