97ba4d5d4def27a1c76b646a9a261925ef31d73d
Melissa Pastore <melissa.pastore@gmail.com>
2021-02-17 20:34:59 -0500

    updated members redux file

--- src/store/members.js
+++ src/store/members.js
@@ -1,28 +1,28 @@
-import axios from "axios"
-import {PROPUBLICA_API} from "../secrets"
+import axios from "axios";
+import { PROPUBLICA_API } from "../secrets";
 
 const SET_MEMBERS = "SET_MEMBERS";
 
 export const setMembers = (members) => ({
   type: SET_MEMBERS,
   members,
-})
+});
 
-
-export function fetchMembers (chamber) {
+export function fetchMembers(chamber) {
   return async (dispatch) => {
     try {
-      const {data} = await axios.get(`https://api.propublica.org/congress/v1/116/${chamber}/members.json`, {headers: {"X-API-KEY": PROPUBLICA_API}}
-      )
-      dispatch (setMembers(data.results[0].members));
+      const {
+        data,
+      } = await axios.get(
+        `https://api.propublica.org/congress/v1/116/${chamber}/members.json`,
+        { headers: { "X-API-KEY": PROPUBLICA_API } }
+      );
+      dispatch(setMembers(data.results[0].members));
+    } catch (err) {
+      console.log("There was a problem finding the members", err);
     }
-catch(err){
-  console.log("There was a problem finding the members", err)
+  };
 }
-  }
-}
-
-
 
 const initialState = [];
 
