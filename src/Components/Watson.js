9f9fac19813a1bf32e0a419a95bc6abd8ed01ff9
Melissa Pastore <melissa.pastore@gmail.com>
2021-02-27 18:16:46 -0500

    updated watson instance

--- src/Components/Watson.js
+++ src/Components/Watson.js
@@ -3,9 +3,9 @@ import React from "react";
 class Watson extends React.Component {
   componentDidMount() {
     window.watsonAssistantChatOptions = {
-      integrationID: "5aacc5ef-b8b7-4610-b21a-ec113c5c2798", // The ID of this integration.
+      integrationID: "befc330d-8f52-4ff1-9b52-af811e567cfd", // The ID of this integration.
       region: "us-south", // The region your integration is hosted in.
-      serviceInstanceID: "01bf79b3-722c-4965-bcba-9d9c8fc09f4e", // The ID of your service instance.
+      serviceInstanceID: "14f1e653-7513-49cf-a2f5-8bf6804d316d", // The ID of your service instance.
       onLoad: function (instance) {
         instance.render();
       },
@@ -16,6 +16,20 @@ class Watson extends React.Component {
         "https://web-chat.global.assistant.watson.appdomain.cloud/loadWatsonAssistantChat.js";
       document.head.appendChild(t);
     });
+    // window.watsonAssistantChatOptions = {
+    //   integrationID: "5aacc5ef-b8b7-4610-b21a-ec113c5c2798", // The ID of this integration.
+    //   region: "us-south", // The region your integration is hosted in.
+    //   serviceInstanceID: "01bf79b3-722c-4965-bcba-9d9c8fc09f4e", // The ID of your service instance.
+    //   onLoad: function (instance) {
+    //     instance.render();
+    //   },
+    // };
+    // setTimeout(function () {
+    //   const t = document.createElement("script");
+    //   t.src =
+    //     "https://web-chat.global.assistant.watson.appdomain.cloud/loadWatsonAssistantChat.js";
+    //   document.head.appendChild(t);
+    // });
   }
 
   render() {
