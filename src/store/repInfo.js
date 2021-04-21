5252bfae00a28019d79346aac4b299d1f97a9016
Melissa Pastore <melissa.pastore@gmail.com>
2021-04-20 22:12:03 -0400

    refactor: fixed repInfo thunk

--- src/store/repInfo.js
+++ src/store/repInfo.js
@@ -20,7 +20,7 @@ export const clearRepInfo = () => ({ type: CLEAR_REP_INFO });
 export function fetchRepInfo(address) {
   return async (dispatch) => {
     try {
-      let response = await axios.get(
+      const { data } = await axios.get(
         `https://www.googleapis.com/civicinfo/v2/representatives`,
         {
           params: {
@@ -29,8 +29,6 @@ export function fetchRepInfo(address) {
           },
         }
       );
-      console.log(response);
-      let { data } = response;
 
       dispatch(setRepInfo(data));
     } catch (error) {
